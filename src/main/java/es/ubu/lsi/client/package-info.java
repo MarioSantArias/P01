e48b9ecd8e26234fa979bb1cd1a81b5c011e869a
Felix <fns0002@alu.ubu.es>
2017-03-30 12:21:34 +0200

    Añadidos los package-info para los paquetes.

--- src/main/java/es/ubu/lsi/client/package-info.java
+++ src/main/java/es/ubu/lsi/client/package-info.java
@@ -1,8 +1,8 @@
 /**
+ * @author Félix Nogal
+ * @author Mario Santamaría
  * 
- */
-/**
- * @author fenos
+ * Paquete que contiene las clases correspondientes al cliente.
  *
  */
 package es.ubu.lsi.client;
--- src/main/java/es/ubu/lsi/common/package-info.java
+++ src/main/java/es/ubu/lsi/common/package-info.java
@@ -1,8 +1,8 @@
 /**
+ * @author Félix Nogal
+ * @author Mario Santamaría
  * 
- */
-/**
- * @author fenos
+ * Paquete que contiene la clase para los mensajes y la enumeracion de los tipos de mensajes.
  *
  */
 package es.ubu.lsi.common;
